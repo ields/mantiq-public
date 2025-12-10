e98cd2f9da74bb518b6bacce36b617c048f39e92
ields <136521453+ields@users.noreply.github.com>
2025-12-10 13:17:55 +0300

    Quartz sync: Dec 10, 2025, 1:17 PM

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -86,11 +86,11 @@ export default ((userOpts?: Partial<Options>) => {
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
-            viewBox="0 0 50 50"
+            viewBox="0 0 24 24"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide-menu"
+            class="hubger-icon" 
           >
             <line x1="4" x2="20" y1="12" y2="12" />
             <line x1="4" x2="20" y1="6" y2="6" />
