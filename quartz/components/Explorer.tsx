dca4a971ebf045a131508cd818a9f047bd480fa3
ields <136521453+ields@users.noreply.github.com>
2025-12-10 16:40:19 +0300

    Quartz sync: Dec 10, 2025, 4:40 PM

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -92,7 +92,7 @@ export default ((userOpts?: Partial<Options>) => {
             stroke-linejoin="round"
             class="lucide-menu" 
           >
-            <line x1="4" x2="20" y1="12" y2="12" />
+            <line x1="9" x2="30" y1="12" y2="12" />
             <line x1="4" x2="20" y1="6" y2="6" />
             <line x1="4" x2="20" y1="18" y2="18" />
           </svg>
