f17493c969c7e25d47456da321eb1252753bac5c
ields <136521453+ields@users.noreply.github.com>
2025-12-10 18:39:50 +0300

    Quartz sync: Dec 10, 2025, 6:39 PM

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -257,7 +257,7 @@ export function renderPage(
     </div>
   )
 
-  const lang = componentData.fileData.frontmatter?.lang ?? cfg.locale?.split("-")[0] ?? "ar"
+  const lang = componentData.fileData.frontmatter?.lang ?? cfg.locale?.split("-")[0] ?? "ar-SA"
   const direction = i18n(cfg.locale).direction ?? "rtl"
   const doc = (
     <html lang={lang} dir={direction}>
