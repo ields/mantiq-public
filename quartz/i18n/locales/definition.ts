0742b3b8e8da7b5ccdf2d69dc464f3b90cc2ada5
ields <136521453+ields@users.noreply.github.com>
2025-12-10 18:45:20 +0300

    Quartz sync: Dec 10, 2025, 6:45 PM

--- quartz/i18n/locales/definition.ts
+++ quartz/i18n/locales/definition.ts
@@ -21,7 +21,7 @@ export interface Translation {
     title: string
     description: string
   }
-  direction?: "rtl" | "rtl"
+  direction?: "rtl" | "ltr"
   components: {
     callout: CalloutTranslation
     backlinks: {
