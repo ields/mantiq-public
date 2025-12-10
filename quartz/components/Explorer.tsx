efc0db0ceef37436fa7d4f088182b224af985072
ields <136521453+ields@users.noreply.github.com>
2025-12-10 17:40:10 +0300

    Quartz sync: Dec 10, 2025, 5:40 PM

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -116,7 +116,7 @@ export default ((userOpts?: Partial<Options>) => {
             stroke-linejoin="round"
             class="fold"
           >
-            <polyline points="18 9 15 12 9 6"></polyline>
+            <polyline points="6 9 12 15 18 9"></polyline>
           </svg>
         </button>
         <div id={id} class="explorer-content" aria-expanded={false} role="group">
@@ -142,7 +142,7 @@ export default ((userOpts?: Partial<Options>) => {
                 stroke-linejoin="round"
                 class="folder-icon"
               >
-                <polyline points="18 9 12 15 6 9"></polyline>
+                <polyline points="6 9 12 15 18 9"></polyline>
               </svg>
               <div>
                 <button class="folder-button">
