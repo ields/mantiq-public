d9f6b41bc37dd5098d612511251713c9ce5fe376
ields <136521453+ields@users.noreply.github.com>
2025-12-11 07:18:40 +0300

    Quartz sync: Dec 11, 2025, 7:18 AM

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -106,9 +106,9 @@ export default ((userOpts?: Partial<Options>) => {
           <h2>{opts.title ?? i18n(cfg.locale).components.explorer.title}</h2>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="14"
-            height="14"
-            viewBox="5 8 14 8"
+            width="16"
+            height="16"
+            viewBox="5 8 16 8"
             fill="none"
             stroke="currentColor"
             stroke-width="2"
