736abc3022904684f7d838b37aaca9f87ac2a527
ields <136521453+ields@users.noreply.github.com>
2026-01-11 17:26:14 +0300

    vault backup: 2026-01-11 17:26:14

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Courier New",
+        header: "sans-serif",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
